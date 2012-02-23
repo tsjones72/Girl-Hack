dd090ab7c7fe9dcb8c1715a3eb79ae84a4ca6473
Tracy <tracyjonesconsulting@gmail.com>
2012-02-22 19:50:29 -0600

    initial Commit

---
+++ lesson1.php
@@ -0,0 +1,18 @@
+<?
+
+?>
+<html>
+	<head>
+		<title>My PHP Form</title>
+	</head>
+	<body><h1>My Form</h1>
+		<form action="calculate.php" method="POST">
+			<label for="num1">Number 1:</label>
+			<input type="text" name="num1" />
+			<label for="num2">Number 2:</label>
+			<input type="text" name="num2" />
+			<input type="submit" value="Add Numbers" />
+		</form>
+	</body>
+
+</html>
